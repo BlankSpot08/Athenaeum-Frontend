c9f20f169eebce8f028bdac1a91760faba483347
scueed <jemuelanacio24@gmail.com>
2021-10-17 17:05:21 +0800

    added Homepage and admin login page components

---
+++ src/components/Homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-btn @click = 'goToAdminLogin'>Login</v-btn>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Homepage',
+
+    methods: {
+      goToAdminLogin() {
+        this.$router.push('/Admin/Login')
+      }
+    }  
+  }
+</script>

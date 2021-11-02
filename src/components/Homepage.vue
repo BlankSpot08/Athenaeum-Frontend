5ccde94055aa5f4b6acd83bb55140e0e465b6a06
Arvin Chu <arvinchu31@gmail.com>
2021-11-02 08:10:54 +0800

    Resolves homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,143 +1,223 @@
 <template>
-  <div id='body'>
-    <div id='upper'>
-        <h1 class='red--text text--accent-4 ml-16'>ATHENAEUM</h1>
-        <v-row class='py-16 my-16'></v-row>
-
-        <h1  class='text-center'>Unlimited Books for education,<br> entertainment, and more.</h1><br>
-        <v-container class='py-10'>
-          <v-row align-content='center' justify='center'>
-            <v-col cols='12' md='4' class='px-2'>
-              <div class='text-center'>             
-                  <v-btn dark to='/admin/login' class='ma-3' elevation='1' height='150px' width='170px' color='red accent-4'>
-                    <v-icon size='220px'>
-                      mdi-account-cog
-                    </v-icon>
-                  </v-btn>                            
-              </div>   
-              <div class='text-center text-h5'>
-                Admin
-              </div>          
-            </v-col>
-            <v-col cols='12'  md='4' class='px-2'>
-              <div class='text-center'>
-                  <v-btn dark  class='ma-3' elevation='1' height='150px' width='170px' color='red accent-4'>
-                    <v-icon size='220px'>  
-                      mdi-account-edit
-                    </v-icon>
-                  </v-btn> 
-              </div>
-              <div class='text-center text-h5'>
-                Student
-              </div>
-            </v-col>
-                       
-          </v-row>
-        </v-container>     
-    </div>
-    <v-row id='dividercolor' class='my-0 py-2'></v-row>
-
-    <div >
-        <v-row  align-content='center' align='center' justify='space-around' class='my-16 py-10' >
-          <v-col cols='12' sm='8' md='4' >
-            <h1 id='titles'>Read Free Library Books.</h1>
-            <p id='description' >We have a lot of books that are available to borrow here.</p>
-          </v-col >
-          <v-col cols='12'  sm='8' md='4'>
-              <v-img src='./assets/homepagebg.jpeg'></v-img>
+  <v-app>
+    <v-main id='body'>
+      <v-container class="pb-11" id='upper' fluid>
+        <v-row class='pb-16 mb-16'>
+          <v-col>
+            <div class='red--text text--accent-4 ml-16 text-h4 font-weight-bold'>ATHENAEUM</div>
           </v-col>
         </v-row>
-      <v-row id='dividercolor' class='my-0 py-2'></v-row>
-        <v-row align='center' justify='space-around' class='my-16 py-10'>
-          <v-col  xs='12' sm='8' md='4'>
-              <v-img src='./assets/homepagebg.jpeg'></v-img>  
-          </v-col>
-          <v-col  xs='12' sm='8' md='4'>
-            <h1 id='titles'>Keep Track of your Favorite Books.</h1> 
-            <p id='description'>Organized books using lists and reading logs.  </p>
+        
+        <v-row class="pt-16">
+          <v-col >
+            <div class='text-center text-h2'>Unlimited Books for education, <br> entertainment, and more.</div>
           </v-col>
         </v-row>
-        <v-row id='dividercolor' class='my-0 py-2'></v-row>
-        <v-row align='center' justify='space-around' class='my-16 py-10'>
-          <v-col xs='12' sm='8' md='4'>
-            <h1 id='titles'>Explore New Books!</h1>
-            <p id='description' >You might find something new that you might interested in.</p>
-          </v-col >
-          <v-col xs='12' sm='8' md='4'>
-              <v-img src='./assets/homepagebg.jpeg' position='center'></v-img>
+        
+        <v-row class='pb-16 mb-16'>
+          <v-col class=' text-right'>
+            <div class=''>             
+              <v-btn dark @click='goToRouterLink("adminLogin")' width="150px" height="60px" x-large class='ma-3' elevation='1' color='red accent-4'> 
+                <v-icon x-large> 
+                  mdi-account-cog
+                </v-icon>
+              </v-btn>                            
+            </div>   
+            <div class='text-h5 mr-12'>
+              Admin
+            </div>          
+          </v-col>
+          
+          <v-col class=' '>
+            <div class=''>
+              <v-btn dark @click='goToRouterLink("studentLogin")' width="150px" height="60px" x-large class='ma-3' elevation='1' color='red accent-4'> 
+                <v-icon x-large>
+                  mdi-account-edit
+                </v-icon>
+              </v-btn> 
+            </div>
+            <div class='text-h5 ml-12'>
+              Student
+            </div>
           </v-col>
         </v-row>
-      
-    </div>
-    <v-row id='dividercolor' class='my-0 py-2'></v-row>
-    <div>
-      <h1 class='mt-16 text-center' style='font-size:40px'>Frequently Asked Questions</h1>
-      <v-row justify='center' class='mb-16 pt-5' >
-        <v-card width='900px' justify='center'>
-          <v-expansion-panels dark style='background-color:black;' >
-            <v-expansion-panel v-for="(item,i) in 5" :key="i" class='mb-2'  >
-              <v-expansion-panel-header style='font-size:30px;'>
-                Title
-              </v-expansion-panel-header>
-              <v-expansion-panel-content >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
+      </v-container>
 
-        </v-card>
-      </v-row>
+      <v-container class="" fluid>
+        <v-row>
+          <v-col class="pa-0 ma-0">      
+            <v-divider id='dividercolor' class="py-1"/>
+          </v-col>
+        </v-row>
 
-    </div>
-  </div>
+        <v-row class=" px-16 py-16 ">
+          <v-col class=" pl-16">
+            <div class=" text-h4 pt-10">
+              Read Free Library Books.<br>
+            </div>
+            <div class=" text-subtitle-1">
+              We have a lot of books that are available to borrow here.
+            </div>
+          </v-col>
+
+          <v-col class=" text-right">
+            <v-img src="../assets/books.jpg" height="250" width="400" max-height="400" max-width="400"/>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="pa-0 ma-0">      
+            <v-divider id='dividercolor' class="py-1"/>
+          </v-col>
+        </v-row>
+
+        <v-row class=" px-16 py-16" justify="right">
+          <v-col class=" justify-end">
+            <v-img src="../assets/favorite_book.jpg" height="250" width="400" max-height="400" max-width="400"/>
+          </v-col>
+
+          <v-col class=" text-right pr-16">
+            <div class=" text-h4 pt-10">
+              Keep Track of your Favorite Books.<br>
+            </div>
+            <div class=" text-subtitle-1">
+              Organized books using lists and reading logs.
+            </div>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="pa-0 ma-0">      
+            <v-divider id='dividercolor' class="py-1"/>
+          </v-col>
+        </v-row>
+
+        <v-row class=" px-16 py-16 ">
+          <v-col class=" pl-16">
+            <div class=" text-h4 pt-10">
+              Explore New Books!<br>
+            </div>
+            <div class=" text-subtitle-1">
+              You might find something new that you might be interested in.
+            </div>
+          </v-col>
+
+          <v-col class="">
+            <v-img src="../assets/explore_book.jpg" max-height="400" max-width="400"/>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="pa-0 ma-0">      
+            <v-divider id='dividercolor' class="py-1"/>
+          </v-col>
+        </v-row>
+
+        <v-row class=" px-16 py-16" justify="center">
+          <v-col class=" text-center" lg="5">
+            <div class=" text-h3 font-weight-bold mb-3">
+              FAQs
+            </div>
+            <v-expansion-panels dark>
+                <v-expansion-panel v-for="(item,i) in faqs" :key="i" class='mb-3'>
+                  <v-expansion-panel-header class="text-body-1">
+                    {{ item.title }}
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content class="text-body-1">
+                    {{ item.description }}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="pa-0 ma-0">      
+            <v-divider id='dividercolor' class="py-1"/>
+          </v-col>
+        </v-row>
+        
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
   export default {
     name: 'Homepage',
+    data() {
+      return {
+        faqs: [
+          { 
+            title: 'What is Athenaeum', 
+            description: '' 
+          },
+          { 
+            title: 'How can I borrow books?', 
+            description: '' 
+          },
+          { 
+            title: 'How can I track my favorite books?', 
+            description: '' 
+          },
+          { 
+            title: 'How can I cancel borrow request?', 
+            description: '' 
+          },
+        ]
+      }
+    },
+    methods: {
+      goToRouterLink(name) {
+        this.$router.push({name: name})
+      }
+    }
   }
 </script>
 
 <style scoped>
-  #dividercolor{
+  #dividercolor {
     background-color: #212121;
   }
-  h1{
+
+  h1 {
     font-size: 60px;
   }
-  #titles{
+
+  #titles {
     font-size: 50px;
     float: left;
   }
-  #description{
+
+  #description {
     font-size: 40px;
     float: left;
   }
 
-  h1,h2, div{
+  h1,h2, div {
     position: relative;
   }
-  #body{
+
+  #body {
     position: relative;
     background-color: black;
     color: white;  
-    min-height: 970px  ;
   }
-  #upper{
+
+  #upper {
     position: relative;
-    min-height: 970px;
   }
-  #upper::before{
+
+  #upper::before {
         content: '';
         position: absolute;
         top:0px;
         right: 0px;
         bottom: 0px;
         left: 0px;
-        background-image: url('./assets/homepagebg.jpeg');
+        background-image: url('../assets/homepagebg.jpeg');
         background-size: 100% 100%;
-        background-position: top center;
         
         opacity: 0.4; 
    }
